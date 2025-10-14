f7c142816bccdab3749ac71f7e108555f195b6d1
shwetap3000 <shwetaprasad30.05@gmail.com>
2025-10-14 13:43:57 +0530

    Add Footer icons

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { SiX } from "react-icons/si";
 import {
   FaInfoCircle,
+  FaInstagram,
+  FaDiscord,
+  FaTelegram,
   FaGithub,
   FaShieldAlt,
   FaFileContract,
@@ -160,6 +163,7 @@ const Footer = () => {
         />
       ),
     },
+
     {
       name: "LinkedIn",
       href: "https://www.linkedin.com/in/sandeepvashishtha/",
@@ -170,6 +174,40 @@ const Footer = () => {
         />
       ),
     },
+
+    {
+      name: "Discord",
+      href: "https://www.discord.com/",
+      icon: (
+        <FaDiscord
+          className="size-10 p-2 rounded-full text-gray-600 dark:text-gray-400 bg-white dark:bg-gray-700 shadow-sm hover:shadow-lg transition-all duration-300 hover:bg-blue-900 hover:text-white dark:hover:bg-blue-900 dark:hover:text-white hover:scale-110 hover:-translate-y-1"
+          size={20}
+        />
+      ),
+    },
+
+    {
+      name: "Telegram",
+      href: "https://www.telegram.com/",
+      icon: (
+        <FaTelegram
+          className="size-10 p-2 rounded-full text-gray-600 dark:text-gray-400 bg-white dark:bg-gray-700 shadow-sm hover:shadow-lg transition-all duration-300 hover:bg-blue-600 hover:text-white dark:hover:bg-blue-600 dark:hover:text-white hover:scale-110 hover:-translate-y-1"
+          size={20}
+        />
+      ),
+    },
+
+    {
+      name: "Instagram",
+      href: "https://www.instagram.com/",
+      icon: (
+        <FaInstagram
+          className="size-10 p-2 rounded-full text-gray-600 dark:text-gray-400 bg-white dark:bg-gray-700 shadow-sm hover:shadow-lg transition-all duration-300 hover:bg-pink-600 hover:text-white dark:hover:bg-pink-600 dark:hover:text-white hover:scale-110 hover:-translate-y-1"
+          size={20}
+        />
+      ),
+    },
+
     
   ];
 
