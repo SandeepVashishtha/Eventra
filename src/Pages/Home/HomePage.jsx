b837c7539e25e99d7fd84dc0b6490edd5f67d215
Janvi Singh <janvisingh6211@gmail.com>
2025-10-31 12:32:11 +0530

    added a testimonial section

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import WhatsHappening from "./components/WhatsHappening";
 import GitHubStats from "./components/GitHubStats";
 import Contributors from "./components/ContributorsCarousel";
+import Testimonials from "./components/Testimonials";
 import HomeCTA from "./components/HomeCTA";
 
 const HomePage = () => {
@@ -11,6 +12,7 @@ const HomePage = () => {
       <Hero />
       <WhatsHappening />
       <GitHubStats />
+      <Testimonials />
       <Contributors />
       <HomeCTA></HomeCTA>
     </>
