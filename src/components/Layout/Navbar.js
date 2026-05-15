1c0eb979181cd105a566855507e9315afeca7477
Soumipal56 <soumiisc2020@gmail.com>
2026-05-15 11:18:54 +0530

    Finalized Navbar complexity reduction and fixed syntax error

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -336,7 +336,24 @@ const MobileUserSection = ({
     </button>
   </div>
 );
-
+const NAV_ITEMS = [
+  { name: "Home", href: "/", icon: <Home className="w-5 h-5" /> },
+  { name: "Events", href: "/events", icon: <Calendar className="w-5 h-5" /> },
+  { name: "Hackathons", href: "/hackathons", icon: <Sparkles className="w-5 h-5" /> },
+  { name: "Projects", href: "/projects", icon: <FolderKanban className="w-5 h-5" /> },
+  {
+    name: "Community",
+    icon: <Users className="w-5 h-5" />,
+    subItems: [
+      { name: "Leaderboard", href: "/leaderBoard", icon: <Trophy className="w-5 h-5" /> },
+      { name: "Contributors", href: "/contributors", icon: <Users className="w-5 h-5" /> },
+      { name: "Contributors Guide", href: "/contributorguide", icon: <Book className="w-5 h-5" /> },
+      { name: "Community Events", href: "/communityEvent", icon: <Users className="w-5 h-5" /> },
+    ],
+  },
+  { name: "About", href: "/about", icon: <Info className="w-5 h-5" /> },
+  { name: "FAQ", href: "/faq", icon: <HelpCircle className="w-5 h-5" /> },
+  { name: "Contact", href: "/contact", icon: <MessageSquare className="w-5 h-5" /> },
 ];
 
 const NavList = ({ location, openDropdown, onToggleGroup, onLinkClick, isMobile }) => (
