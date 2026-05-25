abb11a4a7681e493768431611efdc4f3c3a93aa1
SandeepVashishtha <sandeepvashishtha@outlook.in>
2026-05-25 09:26:39 +0530

    Use createRoot and wrap App with providers
    Switch to React 18's createRoot API and mount the app via root.render. Render the App inside React.StrictMode and wrap it with GlobalErrorBoundary and ThemeProvider so global error handling and theme context are applied. Keep initializeGlobalErrorHandling call and unregister the service worker after mounting. Also clean up minor formatting.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
 import "./index.css";
-
 import App from "./App";
-
 import { ThemeProvider } from "./context/ThemeContext";
 import GlobalErrorBoundary from "./components/common/ErrorBoundary";
 import { initializeGlobalErrorHandling } from "./utils/globalErrorHandler";
@@ -14,7 +11,16 @@ import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 // Initialize Global Runtime Monitoring
 initializeGlobalErrorHandling();
 
-
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
+  <React.StrictMode>
+    <GlobalErrorBoundary>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </GlobalErrorBoundary>
+  </React.StrictMode>
+);
+
+serviceWorkerRegistration.unregister();
