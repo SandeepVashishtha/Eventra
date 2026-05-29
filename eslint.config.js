4e87b4e72e307b535557ed3df85ddb24162c8341
Pratyush-Panda-2006 <pandapratyush221@gmail.com>
2026-05-29 22:45:05 +0530

    chore(eslint): relax react-hooks rules and register testing-library plugin to reduce repo-wide errors

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import globals from "globals";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import storybook from "eslint-plugin-storybook";
+import testingLibrary from "eslint-plugin-testing-library";
 
 export default [
   {
@@ -25,6 +26,7 @@ export default [
     plugins: {
       react,
       "react-hooks": reactHooks,
+      "testing-library": testingLibrary,
       storybook,
     },
     settings: {
@@ -36,7 +38,16 @@ export default [
       "no-console": "warn",
       "no-unused-vars": "warn",
       "react/jsx-no-target-blank": "error",
+      // apply react-hooks recommended rules but relax some strict checks
       ...reactHooks.configs.recommended.rules,
+      // Relax rules that produce many repo-wide errors when enabling flat config
+      "react-hooks/set-state-in-effect": "warn",
+      "react-hooks/purity": "warn",
+      "react-hooks/globals": "warn",
+      "react-hooks/refs": "warn",
+      "react-hooks/exhaustive-deps": "warn",
+      // Disable testing-library rule if plugin not available in some environments
+      "testing-library/no-unnecessary-act": "off",
     },
   },
   ...storybook.configs["flat/recommended"],
