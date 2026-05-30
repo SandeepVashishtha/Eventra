2217b39186ef6d918c1a98668d1aba79e089969d
Vashee khan <vashee.khan.lits@laxmi.edu.in>
2026-05-30 07:03:47 +0530

    refactor: remove unused PUBLIC_URL define

--- vite.config.js
+++ vite.config.js
@@ -43,16 +43,20 @@ export default defineConfig(({ mode }) => {
     },
 
     envPrefix: ["VITE_", "REACT_APP_"],
-    define: {
-      "process.env.NODE_ENV": JSON.stringify(mode),
-      "process.env.PUBLIC_URL": JSON.stringify(""),
-      ...Object.keys(env)
-        .filter((key) => key.startsWith("REACT_APP_") || key.startsWith("VITE_"))
-        .reduce((prev, key) => {
-          prev[`process.env.${key}`] = JSON.stringify(env[key]);
-          return prev;
-        }, {}),
-    },
+  define: {
+  "process.env.NODE_ENV": JSON.stringify(mode),
+
+  ...Object.keys(env)
+    .filter(
+      (key) =>
+        key.startsWith("REACT_APP_") ||
+        key.startsWith("VITE_")
+    )
+    .reduce((prev, key) => {
+      prev[`process.env.${key}`] = JSON.stringify(env[key]);
+      return prev;
+    }, {}),
+},
 
     server: {
       port: 3000,
