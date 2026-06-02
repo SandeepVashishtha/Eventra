e40accecf9b889bd17fb577e4cf193041d794466
Shreyas <shreyasf@icloud.com>
2026-06-02 20:51:46 +0530

    fix: remove corrupted trailing code from api/leaderboard.js (closes #5799)

--- api/leaderboard.js
+++ api/leaderboard.js
@@ -214,6 +214,3 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: "Failed to compile leaderboard data" });
   }
 }
-iled to compile leaderboard data" });
-  }
-}
