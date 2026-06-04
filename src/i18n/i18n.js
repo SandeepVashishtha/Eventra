68bd5767afed7cc06bda984057367306d2b36749
knoxiboy <karankmt.tripathi@gmail.com>
2026-06-04 14:44:25 +0530

    ci: resolve ESM JSON imports and add missing jsdom dependency

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,8 +1,8 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import en from "./locales/en.json";
-import es from "./locales/es.json";
+import en from "./locales/en.json" with { type: "json" };
+import es from "./locales/es.json" with { type: "json" };
 
 i18n
   .use(LanguageDetector)
