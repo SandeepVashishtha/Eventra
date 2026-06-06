39c7c9c8f6bb1cae61016a99d22b12643b439935
Ekjyotkaur07 <ekjyotkaurmalhotra@gmail.com>
2026-06-06 22:19:47 +0530

    fix: correct broken footer links for Community Events and API Docs

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -40,7 +40,7 @@ const footerLinks = {
     },
     {
       name: "Community Events",
-      href: "/communityEvent",
+      href: "/community-event",
       icon: <FaUsers size={14} />,
     },
     {
@@ -88,7 +88,7 @@ const footerLinks = {
     },
     {
       name: "API Docs",
-      href: "/apiDocs",
+      href: "/api-docs",
       icon: <FaBookOpen size={14} />,
     },
   ],
