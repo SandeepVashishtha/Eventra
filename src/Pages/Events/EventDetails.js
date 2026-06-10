eaf5f02a7d7646912e6d12efc2a726a387c616d5
Pratyush-Panda-2006 <pandapratyush221@gmail.com>
2026-06-10 11:16:57 +0530

    fix(lint): resolve compilation, syntax, and react compiler errors to fix ESLint run

--- src/Pages/Events/EventDetails.js
+++ src/Pages/Events/EventDetails.js
@@ -82,7 +82,7 @@ const EventDetails = () => {
         setFetchLoading(false);
       }
     }
-  }, [eventId]);
+  }, [eventId, setEvent, setFetchLoading, setFetchError]);
 
   useEffect(() => {
     loadEvent();
