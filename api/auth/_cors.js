b89ef64229c2acac59445a5b4806bb1071c469c7
PREM SAHITH <premsahith123@gmail.com>
2026-06-14 13:37:15 +0530

    fix(cors): add Access-Control-Allow-Credentials to support cross-origin auth (fixes #8658)

--- api/auth/_cors.js
+++ api/auth/_cors.js
@@ -73,6 +73,7 @@ export const buildCorsHeaders = (req) => {
   const headers = {
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    "Access-Control-Allow-Credentials": "true",
     "Vary": "Origin",
   };
 
