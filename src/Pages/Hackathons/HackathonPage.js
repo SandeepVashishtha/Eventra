177f42ec71bb97773d57491903b22cd21c906b6a
Mayur Sahare <mayursahare2024@gmail.com>
2026-06-28 19:15:35 +0530

    cleanup: replace deprecated BackToTopButton with BackToTop (fixes #9507)

--- src/Pages/Hackathons/HackathonPage.js
+++ src/Pages/Hackathons/HackathonPage.js
@@ -10,7 +10,7 @@ import HackathonCard from "./HackathonCard";
 import HackathonCTA from "./HackathonCTA";
 import Fuse from "fuse.js";
 import { createPortal } from "react-dom";
-import BackToTopButton from "../../components/common/BackToTopButton";
+import BackToTop from "../../components/common/BackToTop";
 // import VirtualizedHackathonGrid from "../../components/common/VirtualizedHackathonGrid";
 import useDocumentTitle from "../../hooks/useDocumentTitle";
 import { filterHackathons } from "./hackathonFilterUtils.mjs";
@@ -798,7 +798,7 @@ const HackathonHub = () => {
       </div>
       
       <HackathonCTA />
-      <BackToTopButton positionClass={positionClass} />
+      <BackToTop positionClass={positionClass} />
     </div>
   );
 };
