11cbe15c7e42bc023790e2d28515f87f5cff329e
kavin553 <kavind@student.tce.edu>
2026-07-01 23:56:53 +0530

    feat : add reading progress bar to event details page

--- src/Pages/Events/EventDetails.js
+++ src/Pages/Events/EventDetails.js
@@ -1,4 +1,5 @@
 import StatusBadge from "../../components/common/StatusBadge";
+import ReadingProgressBar from "../../components/common/ReadingProgressBar";
 import "./EventDetails.print.css";
 import CountdownTimer from "../../components/common/CountdownTimer";
 import { useEffect, useState, useCallback, useRef } from "react";
@@ -310,8 +311,9 @@ const showClosingSoon =
   hoursLeft <= 48;
 
   return (
-    <>
-      <RecentlyViewedTracker event={event} />
+  <>
+    <ReadingProgressBar />
+    <RecentlyViewedTracker event={event} />
       <Helmet>
         <title>{event.title} | Eventra</title>
         <meta property="og:title" content={event.title} />
