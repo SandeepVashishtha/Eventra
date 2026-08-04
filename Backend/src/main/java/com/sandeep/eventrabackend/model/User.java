fb62d28e8efae778a6263330c82cec652a14485c
Aman Kushwaha <amankushwaha61709@gmail.com>
2026-08-04 16:46:45 +0530

    fix: invalidate JWTs after password change

--- Backend/src/main/java/com/sandeep/eventrabackend/model/User.java
+++ Backend/src/main/java/com/sandeep/eventrabackend/model/User.java
@@ -52,4 +52,7 @@ public class User {
     @UpdateTimestamp
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    @Column(name = "password_changed_at")
+    private LocalDateTime passwordChangedAt;
 }
