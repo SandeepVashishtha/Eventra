9222f14f5efdc3ee4773b60e388fa00d00cb1e14
sanrishi <sanrishi@users.noreply.github.com>
2026-08-09 23:22:35 +0530

    feat: add index-of-all utility to eventra-kit

---
+++ src/eventra-kit/__tests__/index-of-all.test.js
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest';
+import * as IndexOfAll from '../index-of-all.js';
+
+describe('index-of-all', () => {
+  it('exports a module', () => {
+    expect(IndexOfAll).toBeDefined();
+  });
+});
+
