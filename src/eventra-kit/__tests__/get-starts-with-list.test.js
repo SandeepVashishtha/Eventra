cdb1e444878422a45f0a8541e54ae2c03c520544
sanrishi <sanrishi@users.noreply.github.com>
2026-08-10 00:19:51 +0530

    feat: add get-starts-with-list utility to eventra-kit

---
+++ src/eventra-kit/__tests__/get-starts-with-list.test.js
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest';
+import * as GetStartsWithList from '../get-starts-with-list.js';
+
+describe('get-starts-with-list', () => {
+  it('exports a module', () => {
+    expect(GetStartsWithList).toBeDefined();
+  });
+});
+
