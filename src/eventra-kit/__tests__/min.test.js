5446a4742d6b93e3271cb1e546d7e0ccd18261cf
sanrishi <sanrishi@users.noreply.github.com>
2026-08-09 23:23:43 +0530

    feat: add min utility to eventra-kit

---
+++ src/eventra-kit/__tests__/min.test.js
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest';
+import * as Min from '../min.js';
+
+describe('min', () => {
+  it('exports a module', () => {
+    expect(Min).toBeDefined();
+  });
+});
+
