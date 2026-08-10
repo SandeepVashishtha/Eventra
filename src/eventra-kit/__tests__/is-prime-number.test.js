1806a9fb0e17cdde52c747369e738b3bc756a462
sanrishi <sanrishi@users.noreply.github.com>
2026-08-10 16:58:40 +0530

    feat: add is-prime-number utility to eventra-kit

---
+++ src/eventra-kit/__tests__/is-prime-number.test.js
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest';
+import * as IsPrimeNumber from '../is-prime-number.js';
+
+describe('is-prime-number', () => {
+  it('exports a module', () => {
+    expect(IsPrimeNumber).toBeDefined();
+  });
+});
+
