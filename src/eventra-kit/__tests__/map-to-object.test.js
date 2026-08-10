18d2d1e497253be39ad4685a5cb6d9f94ef5e203
sanrishi <sanrishi@users.noreply.github.com>
2026-08-10 16:32:05 +0530

    feat: add map-to-object utility to eventra-kit

---
+++ src/eventra-kit/__tests__/map-to-object.test.js
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest';
+import * as MapToObject from '../map-to-object.js';
+
+describe('map-to-object', () => {
+  it('exports a module', () => {
+    expect(MapToObject).toBeDefined();
+  });
+});
+
---
+++ src/eventra-kit/map-to-object.js
@@ -0,0 +1,8 @@
+
+/**
+ * adds a map-to-object helper.
+ */
+export function mapToObject(map) {
+  return Object.fromEntries(map);
+}
+
