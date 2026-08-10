8f9f2168f95f288bb17bc76a233a6fd976e7be70
sanrishi <sanrishi@users.noreply.github.com>
2026-08-10 16:31:22 +0530

    feat: add get-top utility to eventra-kit

---
+++ src/eventra-kit/__tests__/get-top.test.js
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest';
+import * as GetTop from '../get-top.js';
+
+describe('get-top', () => {
+  it('exports a module', () => {
+    expect(GetTop).toBeDefined();
+  });
+});
+
