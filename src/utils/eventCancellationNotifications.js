49f0029f3ae7f0fae23ac2a1abeca42482186681
ionfwsrijan <ionfwsrijan@users.noreply.github.com>
2026-08-11 15:06:29 +0530

    fix(utils): import API_BASE_URL from backendConfig source of truth

--- src/utils/eventCancellationNotifications.js
+++ src/utils/eventCancellationNotifications.js
@@ -7,7 +7,7 @@
  */
 
 import axios from 'axios';
-import { API_BASE_URL } from '../config/api';
+import { API_BASE_URL } from '../config/backendConfig';
 
 class EventCancellationNotificationService {
   constructor() {
