531ca64eb7459144d257e8351c99e5be237fe4fc
sanrishi <sanrishi@users.noreply.github.com>
2026-08-11 22:17:50 +0530

    feat: add average-vector utility to eventra-kit

---
+++ src/eventra-kit/__tests__/average-vector.test.js
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest';
+import * as AverageVector from '../average-vector.js';
+
+describe('average-vector', () => {
+  it('exports a module', () => {
+    expect(AverageVector).toBeDefined();
+  });
+});
+
