ab6910ffd89891299a2ba0cb516fd2d0939bbe50
sanrishi <sanrishi@users.noreply.github.com>
2026-08-13 01:38:49 +0530

    feat: add dedupe-segment utility to eventra-kit

---
+++ src/eventra-kit/__tests__/dedupe-segment.test.js
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest';
+import * as DedupeSegment from '../dedupe-segment.js';
+
+describe('dedupe-segment', () => {
+  it('exports a module', () => {
+    expect(DedupeSegment).toBeDefined();
+  });
+});
+
