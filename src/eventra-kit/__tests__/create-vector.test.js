a807b356d1292cda985ce562cc5f0a2f1c85794b
sanrishi <sanrishi@users.noreply.github.com>
2026-08-13 01:32:47 +0530

    feat: add create-vector utility to eventra-kit

---
+++ src/eventra-kit/__tests__/create-vector.test.js
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest';
+import * as CreateVector from '../create-vector.js';
+
+describe('create-vector', () => {
+  it('exports a module', () => {
+    expect(CreateVector).toBeDefined();
+  });
+});
+
