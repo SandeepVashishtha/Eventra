16f9e89c27052d335d17bb118b782f2fa70b2363
Nancy <9d.24.nancy.sangani@gmail.com>
2026-08-14 19:43:48 +0530

    feat(venue): add WebGPU-accelerated interactive seating map

---
+++ src/components/venue/map/index.js
@@ -0,0 +1,6 @@
+/**
+ * WebGPU-Accelerated Interactive Venue Seating Map Components (#17692)
+ */
+
+export { default as GpuVenueMap } from './GpuVenueMap.jsx';
+export { default as SeatTooltipPanel } from './SeatTooltipPanel.jsx';
