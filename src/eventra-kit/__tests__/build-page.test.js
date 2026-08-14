97d3ebf396df3fe1b32c8f60915f6b994edad950
Akshita <akpandey2307@gmail.com>
2026-08-14 12:11:01 +0530

    fix(kit): buildPage builds pages of items (#18227)

--- src/eventra-kit/__tests__/build-page.test.js
+++ src/eventra-kit/__tests__/build-page.test.js
@@ -1,9 +1,10 @@
 import { describe, it, expect } from 'vitest';
-import * as BuildPage from '../build-page.js';
+import { buildPage } from '../build-page.js';
 
 describe('build-page', () => {
-  it('exports a module', () => {
-    expect(BuildPage).toBeDefined();
+  it('builds pages of items of the given size', () => {
+    expect(buildPage([1, 2, 3, 4, 5], 2)).toEqual([[1, 2], [3, 4], [5]]);
+    expect(buildPage([1, 2, 3, 4], 2)).toEqual([[1, 2], [3, 4]]);
+    expect(buildPage([], 2)).toEqual([]);
   });
 });
-
