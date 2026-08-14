6808d362cb4cc3961f7e418d5ac4f413137eb93d
Xenon010101 <xenon010101@users.noreply.github.com>
2026-08-14 10:31:57 +0530

    fix(kit): import hashStringToNumber in hash-to-index (#17979)

--- src/eventra-kit/hash-to-index.js
+++ src/eventra-kit/hash-to-index.js
@@ -2,6 +2,8 @@
 /**
  * adds a hash index helper.
  */
+import { hashStringToNumber } from './hash-string-to-number.js';
+
 export function hashToIndex(text, bucketCount) {
   return hashStringToNumber(text) % bucketCount;
 }
