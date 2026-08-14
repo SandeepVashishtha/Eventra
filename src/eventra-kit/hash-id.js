19515440bedd9d4f494865f91e7ed6497925b152
Xenon010101 <xenon010101@users.noreply.github.com>
2026-08-14 10:30:52 +0530

    fix(kit): import hashStringToNumber in hash-id (#17978)

--- src/eventra-kit/hash-id.js
+++ src/eventra-kit/hash-id.js
@@ -2,6 +2,8 @@
 /**
  * adds a hash id helper.
  */
+import { hashStringToNumber } from './hash-string-to-number.js';
+
 export function hashId(text) {
   return hashStringToNumber(text).toString(36);
 }
