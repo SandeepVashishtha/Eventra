85ebdcb87b6783a538b824d46fd0afff68a8b988
Xenon010101 <xenon010101@users.noreply.github.com>
2026-08-14 10:34:06 +0530

    fix(kit): import addDaysToDate in past-date (#17981)

--- src/eventra-kit/past-date.js
+++ src/eventra-kit/past-date.js
@@ -2,6 +2,8 @@
 /**
  * adds a past date helper.
  */
+import { addDaysToDate } from './add-days-to-date.js';
+
 export function pastDate(daysAgo) {
   return addDaysToDate(new Date(), -daysAgo);
 }
