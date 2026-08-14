be6eeab6a63ee90986c5dee3edaa286c9d948f0c
suman panda <pandasuman12345@gmail.com>
2026-08-14 10:07:52 +0530

    Live Subtitle Sub-system using Speech Recognition

---
+++ src/components/video/subtitleConfig.js
@@ -0,0 +1,6 @@
+export const defaultSubtitleConfig = {
+  language: "en-US",
+  fontSize: "text-lg", // text-sm, text-base, text-lg, text-xl
+  contrast: "high", // high, standard
+  autoScroll: true,
+};
