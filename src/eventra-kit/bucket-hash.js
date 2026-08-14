cf8d9df29761c2434bf9751f66fd63c934b954c7
Xenon010101 <xenon010101@users.noreply.github.com>
2026-08-14 10:29:52 +0530

    fix(kit): import hashToIndex in bucket-hash (#17977)

--- src/eventra-kit/bucket-hash.js
+++ src/eventra-kit/bucket-hash.js
@@ -2,6 +2,8 @@
 /**
  * adds a bucket hash helper.
  */
+import { hashToIndex } from './hash-to-index.js';
+
 export function bucketHash(text, bucketCount) {
   return hashToIndex(text, bucketCount);
 }
